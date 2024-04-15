8385d5a11bf2152440b84b11bae5b6b64d530b86
Felipe Soares <>
2024-04-15 17:18:35 +0100

    Feat: Create base for JWT Authentication

---
+++ MovieCollection.Server/Extensions/AuthenticationMiddleware.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using MovieCollection.Domain.AccessControl;
+using MovieCollection.Infrastructure.Authentication;
+using MovieCollection.Persistence;
+
+namespace MovieCollection.Server.Extensions
+{
+    public static class AuthenticationMiddleware
+    {
+        public static IServiceCollection AddAuthenticationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var tokenSettings = configuration.GetSection("TokenSettings").Get<TokenSettings>() ?? default!;
+
+            services.AddAuthorizationBuilder();
+
+            services.AddIdentityCore<User>()
+                .AddRoles<IdentityRole<Guid>>()
+                .AddSignInManager()
+                .AddEntityFrameworkStores<MovieCollectionDatabaseContext>()
+                .AddClaimsPrincipalFactory<CustomClaimsPrincipalFactory>();
+
+            services.Configure<DataProtectionTokenProviderOptions>(options =>
+            {
+                options.TokenLifespan = TimeSpan.FromSeconds(tokenSettings.RefreshTokenExpireSeconds);
+            });
+
+            services
+                .AddAuthentication(config =>
+                {
+                    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    config.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(config =>
+                {
+                    config.RequireHttpsMetadata = false;
+                    config.SaveToken = false;   
+                    config.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(tokenSettings.SecretKey)
+                        ),
+                        ValidateIssuer = !string.IsNullOrEmpty(tokenSettings.Issuer),
+                        ValidIssuer = tokenSettings.Issuer,
+                        ValidateAudience = !string.IsNullOrEmpty(tokenSettings.Audience),
+                        ValidAudience = tokenSettings.Audience,
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+
+            return services;
+        }
+    }
+}
--- MovieCollection.Server/Program.cs
+++ MovieCollection.Server/Program.cs
@@ -1,11 +1,30 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using MovieCollection.Domain.AccessControl;
+using MovieCollection.Persistence;
+using MovieCollection.Server.Extensions;
+using Newtonsoft.Json.Converters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddPersistenceServices(builder.Configuration);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(config =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+                     .RequireAuthenticatedUser()
+                     .Build();
+    config.Filters.Add(new AuthorizeFilter(policy));
+})
+    .AddNewtonsoftJson(setup =>
+        setup.SerializerSettings.Converters.Add(new StringEnumConverter()));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthenticationServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -17,6 +36,11 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+else
+{
+    app.UseMigration();
+    app.MapHealthChecks("/healthcheck/readness");
 }
 
 app.UseAuthorization();
