6da30bc665a629ce1d54cd1e6a982abb89f77b70
Felipe Soares <>
2024-04-18 19:39:57 +0100

    Fix: Added model directly in the controller.

--- MovieCollection.Server/Controllers/MovieController.cs
+++ MovieCollection.Server/Controllers/MovieController.cs
@@ -18,16 +18,8 @@ namespace MovieCollection.Server.Controllers
         [ProducesResponseType(typeof(List<MovieResponse>), 200)]
         [ProducesResponseType(typeof(Dictionary<string, string>), 400)]
         [ProducesResponseType(401)]
-        public async Task<IActionResult> Get([FromQuery] string? title, [FromQuery] DateTime? releaseDateStart, [FromQuery] DateTime? releaseDateEnd, [FromQuery] List<Guid>? genres)
+        public async Task<IActionResult> Get([FromQuery] MovieFilters filter)
         {
-            var filter = new MovieFilters()
-            {
-                Title = title,
-                ReleaseDateStart = releaseDateStart,
-                ReleaseDateEnd = releaseDateEnd,
-                Genres = genres ?? new List<Guid>()
-            };
-
             AppResponse<List<MovieResponse>> result = await this.movieService.SearchAsync(filter);
 
             return ValidateResponse(result, HttpStatusCode.OK);
