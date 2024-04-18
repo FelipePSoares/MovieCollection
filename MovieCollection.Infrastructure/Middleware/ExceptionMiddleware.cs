3b5375e3b3f595d68e46aee293d284de94be1c97
FelipePSoares <5514694+FelipePSoares@users.noreply.github.com>
2024-04-19 00:36:45 +0100

    Feat: Added ExceptionMiddleware. (#23)
    Co-authored-by: Felipe Soares <>

---
+++ MovieCollection.Infrastructure/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MovieCollection.Infrastructure.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly IHostEnvironment environment;
+        private readonly ILogger logger;
+
+        public ExceptionMiddleware(RequestDelegate next, IHostEnvironment environment, ILogger logger)
+        {
+            this.next = next;
+            this.environment = environment;
+            this.logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, ex.Message);
+
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
+        {
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new
+            {
+                Message = ex.Message,
+                StackTrace = this.environment.IsDevelopment() ? ex.StackTrace?.ToString() : "Internal Server Error"
+            };
+
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            var json = JsonSerializer.Serialize(response, options);
+
+            await httpContext.Response.WriteAsync(json);
+        }
+    }
+
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomExceptionHandler(
+            this IApplicationBuilder builder, ILogger logger)
+        {
+            return builder.UseMiddleware<ExceptionMiddleware>(logger);
+        }
+    }
+}
